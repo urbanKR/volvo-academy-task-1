5a81fda744c44240f0c0a32ad9f9dfb0d7fcebe2
KrzysztofUrbanX <urbankrzysz36@gmail.com>
2023-11-25 18:03:01 +0100

    Add project files.

---
+++ VolvoAcademyTask1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace VolvoAcademyTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Hello, this is simple calculator! Feel free to use it! (type 'EXIT' to end)***\n");
+            string userInput = "";
+            while (true)
+            {
+                Console.WriteLine("*Please enter first number:*");
+                userInput = Console.ReadLine();
+                if (userInput.ToLower() == "exit")
+                {
+                    break;
+                }
+                if (double.TryParse(userInput, out var firstNumber))
+                {
+                    Console.WriteLine("*Please enter second number:*");
+                    userInput = Console.ReadLine();
+                    if(userInput.ToLower() == "exit")
+                    {
+                        break;
+                    }
+                    if (double.TryParse(userInput, out var secondNumber))
+                    {
+                        double sum = firstNumber + secondNumber;
+                        Console.WriteLine("*Pick math operation to do:*");
+                        Console.WriteLine("'+' - addition\n'-' - subtraction\n'*' - multiplication\n'/' - division\n" +
+                            "'^' - exponentiation\n'!' - factorial");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR: improper second number! Try entering numbers again!\n");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: improper first number! Try entering numbers again!\n");
+                }
+                
+            }
+
+        }
+    }
+}
