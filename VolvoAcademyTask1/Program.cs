b47546bdb188b611b48dd5ceeb33c4bf9d730d75
KrzysztofUrbanX <urbankrzysz36@gmail.com>
2023-11-25 19:47:24 +0100

    Add calculations logic, additional conditions

--- VolvoAcademyTask1/Program.cs
+++ VolvoAcademyTask1/Program.cs
@@ -24,10 +24,56 @@
                     }
                     if (double.TryParse(userInput, out var secondNumber))
                     {
-                        double sum = firstNumber + secondNumber;
                         Console.WriteLine("*Pick math operation to do:*");
                         Console.WriteLine("'+' - addition\n'-' - subtraction\n'*' - multiplication\n'/' - division\n" +
-                            "'^' - exponentiation\n'!' - factorial");
+                            "'^' - exponentiation (first nuber to the power of second number)\n'!' - factorial");
+                        userInput = Console.ReadLine();
+                        if (userInput.ToLower() == "exit")
+                        {
+                            break;
+                        }
+                        
+                        switch(userInput)
+                        {
+                            case "+":
+                                Console.WriteLine($"{firstNumber} + {secondNumber} = {CalculateSum(firstNumber, secondNumber)}");
+                                break;
+                            case "-":
+                                Console.WriteLine($"{firstNumber} - {secondNumber} = {CalculateDifference(firstNumber, secondNumber)}");
+                                break;
+                            case "*":
+                                Console.WriteLine($"{firstNumber} * {secondNumber} = {CalculateProduct(firstNumber, secondNumber)}");
+                                break;
+                            case "/":
+                                Console.WriteLine($"{firstNumber} / {secondNumber} = {CalculateQuotient(firstNumber, secondNumber)}");
+                                break;
+                            case "^":
+                                Console.WriteLine($"{firstNumber} ^ {secondNumber} = {CalculateExponential(firstNumber, secondNumber)}");
+                                break;
+                            case "!":
+                                if(IsInteger(firstNumber) && firstNumber >= 0)
+                                {
+                                    Console.WriteLine($"{firstNumber}! = {CalculateFactorial(firstNumber)}");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"{firstNumber} is not an Integer or it is negative, so we can not calculate factorial!\n");
+                                }
+                                if (IsInteger(secondNumber) && secondNumber >= 0)
+                                {
+                                    Console.WriteLine($"{secondNumber}! = {CalculateFactorial(secondNumber)}");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"{secondNumber} is not an Integer or it is negative, so we can not calculate factorial!\n");
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("ERROR: improper math operation! Try entering numbers and correct operation again!\n");
+                                break;
+                          
+
+                        }
                     }
                     else
                     {
@@ -42,6 +88,42 @@
                 
             }
 
+        }
+        static double CalculateSum(double x, double y)
+        {
+            return x + y;
+        }
+        static double CalculateDifference(double x, double y)
+        {
+            return x - y;
+        }
+        static double CalculateProduct(double x, double y)
+        {
+            return x * y;
+        }
+        static double CalculateQuotient(double x, double y)
+        {
+            return x / y;
+        }
+        static double CalculateExponential(double x, double y)
+        {
+            return Math.Pow(x, y);
+        }
+        static double CalculateFactorial(double x)
+        {
+            if(x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * CalculateFactorial(x - 1);
+            }
+            
+        }
+        static bool IsInteger(double x)
+        {
+            return x == Math.Floor(x);
         }
     }
 }
